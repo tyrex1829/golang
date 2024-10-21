d549423bbaf353fe72e0b10b358d558201cf9a18
Saksham Khare <sakshamkhare19@gmail.com>
2024-10-21 15:00:14 +0530

    loops & printing without fmt

--- main.go
+++ main.go
@@ -1,19 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-)
+func main() {
+	// const a int = 40
+	// const b float64 = 3.14
+	// const c bool = true
+	// const d string = "hello, Go devs!"
 
-func main()  {
-	const a int = 40
-	const b float64 = 3.14
-	const c bool = true
-	const d string = "hello, Go devs!"
+	// fmt.Println("hello world " + "Random Number", rand.Int31n(10))
+	// fmt.Println("Integer: ", a)
+	// fmt.Println("Float: ", b)
+	// fmt.Println("Boolean: ", c)
+	// fmt.Println("String: ", d)
 
-	fmt.Println("hello world " + "Random Number", rand.Int31n(10))
-	fmt.Println("Integer: ", a)
-	fmt.Println("Float: ", b)
-	fmt.Println("Boolean: ", c)
-	fmt.Println("String: ", d)
+	sum := 0
+	for i := 1; i < 3; i++ {
+		sum += i
+	}
+
+	println(sum)
 }
