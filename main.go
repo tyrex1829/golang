9d265449b1d7d9147cedd66f1ed7b395927c459a
Saksham Khare <sakshamkhare19@gmail.com>
2024-10-21 14:53:01 +0530

    constants rather than var

--- main.go
+++ main.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main()  {
-	a := 40
-	b := 3.14
-	c := true
-	d := "hello, Go devs!"
+	const a int = 40
+	const b float64 = 3.14
+	const c bool = true
+	const d string = "hello, Go devs!"
 
 	fmt.Println("hello world " + "Random Number", rand.Int31n(10))
 	fmt.Println("Integer: ", a)
