f1413633523eae79a0ac0c88f6c3d901d793f6c2
Saksham Khare <sakshamkhare19@gmail.com>
2024-10-21 14:47:50 +0530

    use variables

--- main.go
+++ main.go
@@ -6,5 +6,14 @@ import (
 )
 
 func main()  {
+	var a int = 40
+	var b float64 = 3.14
+	var c bool = true
+	var d string = "hello, Go devs!"
+
 	fmt.Println("hello world " + "Random Number", rand.Int31n(10))
+	fmt.Println("Integer: ", a)
+	fmt.Println("Float: ", b)
+	fmt.Println("Boolean: ", c)
+	fmt.Println("String: ", d)
 }
